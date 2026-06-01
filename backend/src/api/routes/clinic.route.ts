15f4f23d075091b36e83394920b49ae668bed38b
Saad Sayyed <saadsyed950@gmail.com>
2026-06-01 19:46:58 +0530

    Fetch clinic user route

--- backend/src/api/routes/clinic.route.ts
+++ backend/src/api/routes/clinic.route.ts
@@ -1,8 +1,15 @@
 import { Router } from "express";
 import * as controllers from "../controllers/clinic.controller";
+import { requireAuth } from "@clerk/express";
 
 const clinicRouter = Router();
 
 clinicRouter.post("/account/sync", controllers.syncClinicUserController);
+
+clinicRouter.get(
+  "/account",
+  requireAuth(),
+  controllers.syncClinicUserController,
+);
 
 export default clinicRouter;
