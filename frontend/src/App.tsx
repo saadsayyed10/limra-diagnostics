cb636517cdd9c3ff6f66c8a9d4d2e2e7bcc135a1
Saad Sayyed <saadsyed950@gmail.com>
2026-06-01 16:35:27 +0530

    Clerk setup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,16 @@
+import { Show, SignInButton, SignOutButton } from "@clerk/react";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className="flex justify-center items-center w-full min-h-screen">
+      <Show when={"signed-out"}>
+        <SignInButton mode="modal" />
+      </Show>
+      <Show when={"signed-in"}>
+        <SignOutButton />
+      </Show>
+    </div>
+  );
 };
 
 export default App;
