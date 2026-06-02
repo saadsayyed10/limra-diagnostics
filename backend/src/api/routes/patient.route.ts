3771ea4442c57ba7d49ae149e36f5be1d40f39d1
Saad Sayyed <saadsyed950@gmail.com>
2026-06-02 11:42:51 +0530

    Fetch all patients route

--- backend/src/api/routes/patient.route.ts
+++ backend/src/api/routes/patient.route.ts
@@ -1,12 +1,20 @@
 import { Router } from "express";
 import * as controller from "../controllers/patient.controller";
+import { requireAuth } from "@clerk/express";
 
 const patientRouter = Router();
 
 patientRouter.post(
   "/register/regular",
+  requireAuth(),
   controller.registerRegularPatientController,
 );
-patientRouter.post("/register/obs", controller.registerOBSPatientController);
+patientRouter.post(
+  "/register/obs",
+  requireAuth(),
+  controller.registerOBSPatientController,
+);
+
+patientRouter.get("/all", controller.fetchAllPatientsController);
 
 export default patientRouter;
