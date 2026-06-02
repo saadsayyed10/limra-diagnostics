41db6c93b6c785f3ead5f1123b91fa0dff3d8fc3
Saad Sayyed <saadsyed950@gmail.com>
2026-06-02 11:11:01 +0530

    Regular patient register service

---
+++ backend/src/api/services/patient.service.ts
@@ -0,0 +1,35 @@
+import { PatientType } from "@prisma/client";
+import prisma from "../../lib/orm";
+
+export const registerRegularPatient = async (
+  name: string,
+  phone: string,
+  age: number,
+  address: {
+    localAddress: string;
+    pincode: string;
+    city: string;
+    state: string;
+  },
+) => {
+  const existing = await prisma.patients.findUnique({
+    where: {
+      phone,
+    },
+  });
+  if (existing) {
+    throw new Error("Patient account already exists");
+  }
+
+  const patient = await prisma.patients.create({
+    data: {
+      name,
+      phone,
+      age,
+      address,
+      patientType: PatientType.REGULAR,
+    },
+  });
+
+  return patient;
+};
