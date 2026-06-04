21d02258b01e4bc2c95774dff0a253e1fcbda6e4
Saad Sayyed <saadsyed950@gmail.com>
2026-06-04 18:34:58 +0530

    Fetch single patient controller

--- backend/src/api/controllers/patient.controller.ts
+++ backend/src/api/controllers/patient.controller.ts
@@ -118,6 +118,30 @@ export const fetchAllPatientsController = async (
   }
 };
 
+export const fetchSinglePatientServiceController = async (
+  req: Request,
+  res: Response,
+) => {
+  const { id } = req.params;
+
+  try {
+    //   const { userId } = getAuth(req);
+    //   if (!userId) {
+    //     let errorMessage = "Unauthorized: Invalid token";
+    //     console.log(errorMessage);
+    //     return res.status(401).json({ error: errorMessage });
+    //   }
+
+    const patient = await patientService.fetchSinglePatientService(
+      id as string,
+    );
+    res.status(200).json({ patient });
+  } catch (error: any) {
+    console.log(error.message);
+    return res.status(400).json({ error: error.message });
+  }
+};
+
 export const deletePatientController = async (req: Request, res: Response) => {
   const { id } = req.params;
 
