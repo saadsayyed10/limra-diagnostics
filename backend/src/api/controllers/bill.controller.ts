049e0b6186aa35cac9ca76e5502688f23ff0b48a
Saad Sayyed <saadsyed950@gmail.com>
2026-06-05 12:29:51 +0530

    Update bill controller

--- backend/src/api/controllers/bill.controller.ts
+++ backend/src/api/controllers/bill.controller.ts
@@ -81,3 +81,47 @@ export const fetchSingleBillController = async (
     return res.status(400).json({ error: error.message });
   }
 };
+
+export const updateBillController = async (req: Request, res: Response) => {
+  const { id } = req.params;
+
+  const { scanType, totalAmount, dueAmount, concession, docxUrl, patientId } =
+    req.body;
+
+  const data = {
+    scanType,
+    totalAmount,
+    dueAmount,
+    concession,
+    docxUrl,
+    patientId,
+  };
+  if (!data) {
+    const errorMessage = "Required fields are missing";
+    console.log(errorMessage);
+    return res.status(400).json({ error: errorMessage });
+  }
+
+  try {
+    // const { userId } = getAuth(req);
+    // if (!userId) {
+    //   let errorMessage = "Unauthorized: Invalid token";
+    //   console.log(errorMessage);
+    //   return res.status(401).json({ error: errorMessage });
+    // }
+
+    const bill = await billService.updateBillService(
+      id as string,
+      scanType,
+      totalAmount,
+      dueAmount,
+      concession,
+      docxUrl,
+      patientId,
+    );
+    res.status(200).json({ message: "Bill updated", bill });
+  } catch (error: any) {
+    console.log(error.message);
+    return res.status(400).json({ error: error.message });
+  }
+};
